9faba3eff1f3513287bef2fa3c7479e71b12483f
Gergely Imreh <gergely@balena.io>
2019-11-05 20:25:00 +0000

    Update based on feedback on PR
    Signed-off-by: Gergely Imreh <gergely@balena.io>

--- appmigrator_template.sh
+++ appmigrator_template.sh
@@ -9,11 +9,35 @@ source /etc/profile
 # shellcheck disable=SC1091
 source /usr/sbin/resin-vars
 
+# Global tunables (turned into real variables before being run)
 TOKEN="%%TOKEN%%"
 TARGET_APP_ID="%%TARGET_APP_ID%%"
 FROM_VOLUME="%%FROM_VOLUME%%"
 TO_VOLUME="%%TO_VOLUME%%"
 
+# Global static parameters
+BACKUP_FILE="/mnt/data/backup.tgz"
+
+# Finding the right balenaEngine binary
+engines=("balena-engine" "balena" "docker")
+for e in "${engines[@]}" ; do
+    if command "${e}" >/dev/null 2>&1; then
+        ENGINE="${e}"
+        break
+    fi
+done
+
+###
+# Helper functions
+###
+
+#######################################
+# Set up logging functions
+# Globals:
+#   tail_pid
+# Arguments:
+#   workdir: the working directory
+#######################################
 setup_logfile() {
     local workdir=$1
     LOGFILE="${workdir}/appmigrator.$(date +"%Y%m%d_%H%M%S").log"
@@ -25,6 +49,13 @@ setup_logfile() {
     exec 1>> "$LOGFILE" 2>&1
 }
 
+#######################################
+# Clean up when we reach a finished state
+# Globals:
+#   tail_pid
+# Arguments:
+#   failure: the failure message
+#######################################
 finish_up() {
     local failure=$1
     local exit_code=0
@@ -40,6 +71,26 @@ finish_up() {
 }
 
 #######################################
+# A GET call to the relevant balenaCloud API
+# Globals:
+#   API_ENDPOINT
+#   TOKEN
+# Arguments:
+#   query: the query to make, e.g. "/ping" or "/v4/device"...
+# Returns:
+#   the results of the API call
+#######################################
+api_get() {
+    local query=$1
+    curl --silent --fail --retry 10 -X GET \
+        -H "Authorization: Bearer ${TOKEN}" \
+        -H "Content-Type: application/json" \
+        "${API_ENDPOINT}${query}"
+}
+
+#######################################
+# Load resin vars manually when "resin-vars" doesn't work
+# as expected on older OS versions
 # Globals:
 #   CONFIG_PATH
 # Arguments:
@@ -62,6 +113,10 @@ resin_var_manual_load() {
     echo "${var_value}"
 }
 
+
+###
+# The main migrator
+###
 main() {
     local current_release_info
     local target_release_info
@@ -83,10 +138,7 @@ main() {
     fi
 
     # Application ID check
-    APPLICATION_ID=$(curl --silent --fail --retry 5 -X GET \
-    "${API_ENDPOINT}/v4/device(${DEVICE_ID})" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer ${TOKEN}" | jq -r '.d[0].belongs_to__application.__id')
+    APPLICATION_ID=$(api_get "/v4/device(${DEVICE_ID})" | jq -r '.d[0].belongs_to__application.__id')
     if [ -z "${APPLICATION_ID}" ]; then
         finish_up "Couldn't get Application ID"
     fi
@@ -94,10 +146,7 @@ main() {
         finish_up "Current application is the same as the target."
     fi
 
-    current_release_info=$(curl --silent --fail --retry 5 -X GET \
-    "${API_ENDPOINT}/v4/release?\$filter=belongs_to__application%20eq%20${APPLICATION_ID}" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $TOKEN") || finish_up "Couldn't get release data."
+    current_release_info=$(api_get "/v4/release?\$filter=belongs_to__application%20eq%20${APPLICATION_ID}") || finish_up "Couldn't get release data."
     # If the user has no access to the app, the reply will be empty
     local -a current_volumes=()
     while IFS='' read -r volume; do
@@ -121,10 +170,7 @@ main() {
         finish_up "Couldn't select volume to back up from."
     fi
 
-    target_release_info=$(curl --silent --fail --retry 5 -X GET \
-    "${API_ENDPOINT}/v4/release?\$filter=belongs_to__application%20eq%20${TARGET_APP_ID}" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $TOKEN") || finish_up "Couldn't get release data."
+    target_release_info=$(api_get "/v4/release?\$filter=belongs_to__application%20eq%20${TARGET_APP_ID}") || finish_up "Couldn't get release data."
     # If the user has no access to the app, the reply will be empty
     local -a target_volumes=()
     while IFS='' read -r volume; do
@@ -154,31 +200,26 @@ main() {
 
     echo "Stopping supervisor and user application"
     systemctl stop resin-supervisor || true
-    if command balena >/dev/null 2>&1 ; then
-        ENGINE="balena"
-    else
-        ENGINE="docker"
-    fi
     # shellcheck disable=SC2046
     $ENGINE rm -f $($ENGINE ps -a -q) 2>/dev/null || true
 
     # Pre-backup clean just in case
     rm -rf "./backup" || finish_up "Couldn't clear temporary backup location."
     mkdir -p ./backup || finish_up "Couldn't create temporary backup location."
-    rm -rf /mnt/data/backup.tgz || finish_up "Couldn't clear up backup file location."
+    rm -rf "${BACKUP_FILE}" || finish_up "Couldn't clear up backup file location."
 
     echo "Backing up."
     cp -r "/var/lib/docker/volumes/${APPLICATION_ID}_${FROM_VOLUME}/_data/" "./backup/${TO_VOLUME}/" || finish_up "Couldn't copy data from docker volume ${APPLICATION_ID}_${FROM_VOLUME}."
     # The command required like this for the way supervisor extraction works, needing "." being present in the archive as root folder
-    tar -czvf /mnt/data/backup.tgz  -C backup . || finish_up "Couldn't create backup file."
+    tar -czvf "${BACKUP_FILE}"  -C backup . || finish_up "Couldn't create backup file."
     rm -rf "./backup" || finish_up "Couldn't clear temporary backup location after backup file creation."
 
     echo "Moving device to new application."
     curl --silent --fail --retry 10 -X PATCH \
-    "${API_ENDPOINT}/v4/device(${DEVICE_ID})" \
-    -H "Content-Type: application/json" \
-    -H "Authorization: Bearer ${TOKEN}" \
-    --data '{ "belongs_to__application": "'"${TARGET_APP_ID}"'" }' || finish_up "Failed to move device to the new app, ID ${TARGET_APP_ID}"
+        "${API_ENDPOINT}/v4/device(${DEVICE_ID})" \
+        -H "Content-Type: application/json" \
+        -H "Authorization: Bearer ${TOKEN}" \
+        --data '{ "belongs_to__application": "'"${TARGET_APP_ID}"'" }' || finish_up "Failed to move device to the new app, ID ${TARGET_APP_ID}"
 
     systemctl restart resin-supervisor || finish_up "Supervisor restart didn't work."
     sleep 10 # Let the restart commence
